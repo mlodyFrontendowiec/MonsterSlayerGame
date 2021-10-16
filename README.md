bb6a1cb1f6c0af469d8fac8f3a8c2c06677d1e0d
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-10-16 16:14:16 +0200

    Add attack  functions

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # MonsterSlayerGame
-# MonsterSlayerGame
--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const app = Vue.createApp({
   data() {
     return { playerHealth: 100, monsterHealth: 100 };
   },
-  methods: {},
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+    },
+  },
 });
 
 app.mount("#game");
