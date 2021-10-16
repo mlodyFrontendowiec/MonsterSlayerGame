3e46ad1653a5e8a83bd92cd2ab53cca1bf5a0d61
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-10-16 20:09:19 +0200

    Game over in game

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 function getRandomValue(min, max) {
+  console.log(Math.floor(Math.random() * (max - min)) + min);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const app = Vue.createApp({
   data() {
-    return { playerHealth: 100, monsterHealth: 100, currentRound: 0 };
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      currentRound: 0,
+      winner: null,
+    };
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth + "%" };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.playerHealth + "%" };
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "player";
+      }
     },
   },
   methods: {
@@ -39,7 +70,6 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healValue;
       }
-      this.attackPlayer();
     },
   },
 });
